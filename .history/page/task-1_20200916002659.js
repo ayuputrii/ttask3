62b59ae552b145a8da518bc00213333081cd4571
ayuputrii <ayuarmadani.pm@gmail.com>
2020-09-16 01:15:01 +0700

    first commit

--- .history/page/task-1_20200916002659.js
+++
@@ -1,8 +0,0 @@
-const cekHariKerja = (day) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const dataDay = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat"]
-        let cek = dataDay.find((item) => {
-            return item = day
-        })
-    })
-})
--- .history/page/task-5-1_20200916011029.js
+++
@@ -1,8 +0,0 @@
-// 1. Deteksi Palindrom
-
-var input = "Malam";
-if (input == "Malam") {
-    console.log("Palindrom")
-} else {
-    console.log("bukan Palindrom")
-}
